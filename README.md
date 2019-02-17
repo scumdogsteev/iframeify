9f6f960942eddb5aaf63f2040f8f8c8a8baa7da2
Steve Myles <scumdogsteev@users.noreply.github.com>
2019-02-17 16:43:36 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+* **Who:**  [Steve Myles][home] wrote this
+* **What:**  using iframes to read markdown files into [tumblr](https://tumblr.com/) pages 
+* **When:**  February 2019
+* **Where:**  [GitHub repo][repo]
+* **Why:**  tumblr won't play nice when I paste in html 
+* **How:**  a simple [R script][script]; see the [blog post][post] for more detail
+
+[post]:https://s.mylesandmyles.info/blog/2019/02/11/iframeify/
+[home]:https://s.mylesandmyles.info/
+[tumblr]:https://tumblr.com/
+[repo]:https://github.com/scumdogsteev/iframeify
+[script]:https://github.com/scumdogsteev/iframeify/blob/master/iframeify.R
+[license]:https://github.com/scumdogsteev/iframeify/blob/master/LICENSE
---
+++ iframeify.R
@@ -0,0 +1,46 @@
+## iframeify.R
+## by Steve Myles / https://s.mylesandmyles.info/
+## 10 February 2019
+##
+## converts a markdown file into an iframe-able html file for embedding in
+## another site such as tumblr
+
+## load knitr and stringr packages
+library(knitr)
+library(stringr)
+
+iframeify <- function(filename, stylesheet = "styles.css", styles = "styles.txt") {
+
+    ## add markdown and html extensions to filename
+    md_filename <- paste0(filename, ".md")
+    html_filename <- paste0(filename, ".html")
+
+    ## check whether stylesheet exists; if not, set to blank
+    if (!file.exists(stylesheet))
+      stylesheet = ""
+
+    ## knit
+    knit2html(md_filename, html_filename, stylesheet = stylesheet)
+
+    ## read the html and styles files into memory; if styles does not exist,
+    ## replace it with opening html <style> tag
+    html <- readLines(html_filename)
+    if (file.exists(styles)) {
+      styles <- paste(readLines(styles), collapse = "\n")
+    } else {
+      styles = '<style type="text/css">'
+    }
+
+    ## replace the title, add the styles, and add 'target="_parent"' to html tags
+    html <- html %>%
+        str_c() %>%
+        str_replace_all(c('<title>(.*?)</title>' = paste0('<title>', filename, '</title>'),
+                  '<style type="text/css">' = styles,
+                  'a href' = 'a target="_parent" href'))
+
+    ## write results back to the html file
+    writeLines(html, con = html_filename)
+
+    ## remove interim .txt file
+    file.remove(paste0(filename, ".txt"))
+}
